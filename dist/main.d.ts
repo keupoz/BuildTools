8532342d69c8f0e6fac91a4ee9e5d8db77ea2d48
keupoz <keupoz@gmail.com>
2019-07-22 00:38:52 +0700

    Bump version

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -8,3 +8,4 @@ export { uglify } from './RollupPlugins';
 export { AssetsConfig } from './bundlers/Assets';
 export { RollupPlugins, Assets, Rollup, Sass, Pug };
 export { GulpHelper };
+export declare function task<Callback extends () => Promise<void>>(name: string, fn: Callback): Callback;
