a0fa46461947bda4210d15f4370ce1234a519fe9
keupoz <keupoz@gmail.com>
2019-10-06 17:49:52 +0700

    Changed input options of Pug bundler

--- src/bundlers/Pug.ts
+++ src/bundlers/Pug.ts
@@ -1,32 +1,49 @@
-import { writeFileSync } from "fs";
+import { ensureDirSync, writeFileSync } from "fs-extra";
+import { sync as globSync } from "glob";
+import { join, parse, resolve } from "path";
 import { Options, renderFile } from "pug";
 import AbstractBundler from "./AbstractBundler";
 
 export type PugOptions = Options & {
-    filename: string;
-    output: string;
+    inputdir: string;
+    outputdir: string;
 };
 
 export default class Pug extends AbstractBundler {
-    private output: string;
+    private inputdir: string;
+    private outputdir: string;
     private config: PugOptions;
 
     constructor(config: PugOptions, watch: boolean, autobundle?: boolean) {
         super();
 
-        this.output = config.output;
-        delete config.output;
+        this.inputdir = config.inputdir;
+        this.outputdir = config.outputdir;
+
+        delete config.inputdir;
+        delete config.outputdir;
 
         this.config = config;
 
         if (watch) {
-            this.initWatcher(config.filename, autobundle);
+            this.initWatcher(join(this.inputdir, "/*.pug"), autobundle);
         }
     }
 
-    public async bundle(): Promise<void> {
-        let result = renderFile(this.config.filename!, this.config);
+    public async bundle(path?: string): Promise<void> {
+        if (!path) {
+            let paths = globSync(join(this.inputdir, "/*.pug"));
+            for (let p of paths) await this.bundle(p);
+            return;
+        }
 
-        writeFileSync(this.output, result);
+        let path_obj = parse(path),
+            result = renderFile(path, this.config);
+
+        let outputdir = resolve(this.outputdir, path_obj.base == "index.pug" ? "" : path_obj.name),
+            output = join(outputdir, "/index.html");
+
+        ensureDirSync(outputdir);
+        writeFileSync(output, result);
     }
 }
